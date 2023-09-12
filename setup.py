acb1223fd8fe975fbeb42a14a7ca0c5148f6f1ff
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2023-09-12 17:33:14 +1000

    cookiecutter project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building, exploring and visualising an Expected Valuing Actions by Estimating Probabilities model.',
+    author='Ciaran Grant',
+    license='MIT',
+)
