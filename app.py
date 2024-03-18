de6c6b05a268da65fbc111dc8c38942a359c87cc
Ciaran Grant <ciaran.grant@hotmail.co.uk>
2024-03-18 12:23:07 +1100

    Correct CG_Expected_Score name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def predict(ID = None):
     
     chains = load_data(Dataset_Name="AFL_API_Match_Chains", ID = request.json['ID'])
-    shots = load_data(Dataset_Name='CG_Expected_Scores', ID = request.json['ID'])
+    shots = load_data(Dataset_Name='CG_Expected_Score', ID = request.json['ID'])
     
     preprocessor = load_preprocessor()
     scores_model = load_scores_model()
@@ -21,9 +21,9 @@ def predict(ID = None):
 
     data = calculate_exp_vaep_values(schema_chains) 
     
-    # upload_data(Dataset_Name="CG_Expected_VAEP", Dataset=data, overwrite=True, update_if_identical=True)
+    upload_data(Dataset_Name="CG_Expected_VAEP", Dataset=data, overwrite=True, update_if_identical=True)
     
     return data.to_json(orient='records')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8002, debug=False)
+    app.run(host="0.0.0.0", port=8002, debug=True)
